068e276f8c1f31b4aebaa4383b9da0271da634a4
Linkdom <donglinkang2021@163.com>
2024-05-23 21:40:01 +0800

    add imagenet pretrained model checkpoint

--- save_model.py
+++ save_model.py
@@ -1,7 +1,5 @@
 import torch
 import lightning as L
-from lightning.pytorch.loggers import TensorBoardLogger
-from lightning.pytorch.strategies import DeepSpeedStrategy
 from light import *
 from datasets import *
 from config import *
@@ -13,14 +11,14 @@ torch.set_float32_matmul_precision('medium')
 if __name__ == '__main__':
 
     # lightning deepspeed has saved a directory instead of a file
-    save_path = "logs/VQVAE_finetune_CIFAR10/version_1/checkpoints/epoch=9-step=790.ckpt"
-    output_path = "ckpt/VQVAE_finetune_CIFAR10_lightning.pt"
+    save_path = "logs/VQVAE_unmask_ImageNet/version_3/checkpoints/epoch=9-step=5010.ckpt"
+    output_path = "ckpt/VQVAE_imagenet_lightning.pt"
     convert_zero_checkpoint_to_fp32_state_dict(save_path, output_path)
 
-    # model = VQVAELightning.load_from_checkpoint(
-    #     "ckpt/VQVAE_CIFAR10.pt", 
-    #     hparams_file="logs/VQVAE_CIFAR10/version_8/hparams.yaml"
-    # )
-    # vqvae = VQVAE(**model_kwargs)
-    # vqvae.load_state_dict(model.vqvae.state_dict())
-    # torch.save(vqvae.state_dict(), "ckpt/unmask_vqvae_cifar10_10epo.pth")
+    model = VQVAEUnmaskLightning.load_from_checkpoint(
+        "ckpt/VQVAE_imagenet_lightning.pt", 
+        hparams_file="logs/VQVAE_unmask_ImageNet/version_3/hparams.yaml"
+    )
+    vqvae = VQVAE(**model_kwargs)
+    vqvae.load_state_dict(model.vqvae.state_dict())
+    torch.save(vqvae.state_dict(), "ckpt/unmask_vqvae_imagenet_10epo.pth")
