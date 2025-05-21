902bb8d80e7b680a279042e47d7cf5347869dba1
linkdom <donglinkang2021@163.com>
2025-05-21 22:49:35 +0800

    run causal method ; run scale 1/8 feature method

--- run.sh
+++ run.sh
@@ -4,18 +4,27 @@ conda activate linkdom
 export CUDA_VISIBLE_DEVICES=0,1
 python train.py task.model.quantize._target_="minvqvae.core.SoftmaxQuantize"
 python train.py task.model.quantize._target_="minvqvae.core.FFN"
+python train.py task.model.quantize._target_="minvqvae.core.AttentionQuantize"
 
 conda activate linkdom
 export CUDA_VISIBLE_DEVICES=2,3
 python train.py task.model.quantize._target_="minvqvae.core.ArgmaxQuantize"
 python train.py task.model.quantize._target_="minvqvae.core.LLMFFN"
+python train.py \
+    task.model.quantize._target_="minvqvae.core.SoftmaxQuantize" \
+    task.model.scale_factor=2
 
 conda activate linkdom
 export CUDA_VISIBLE_DEVICES=4,5
 python train.py task.model.quantize._target_="minvqvae.core.SoftQuantize"
 python train.py task.model.quantize._target_="minvqvae.core.Identity"
+python train.py \
+    task.model.quantize._target_="minvqvae.core.Identity" \
+    task.model.scale_factor=2
 
 conda activate linkdom
 export CUDA_VISIBLE_DEVICES=6,7
 python train.py task.model.quantize._target_="minvqvae.core.SimpleQuantize"
-
+python train.py \
+    task.model.quantize._target_="minvqvae.core.AttentionQuantize" \
+    task.model.scale_factor=2
